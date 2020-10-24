3506324db92d8e786576b79c4998e53e73cea34f
Schetinnikov Stas <schetinnikov@gmail.com>
2020-10-24 19:00:23 +0300

    Added configuration

---
+++ configuration/hello-py/app.py
@@ -0,0 +1,39 @@
+import os
+import json
+
+from flask import Flask
+
+app = Flask(__name__)
+
+config = {
+    'DATABASE_URI': os.environ.get('DATABASE_URI', ''),
+    'HOSTNAME': os.environ['HOSTNAME'],
+    'GREETING': os.environ.get('GREETING', 'Hello'),
+}
+
+@app.route("/")
+def hello():
+    return config['GREETING'] + ' from ' + config['HOSTNAME'] + '!'
+
+@app.route("/config")
+def configuration():
+    return json.dumps(config)
+
+
+@app.route("/version")
+def version():
+    return {"version": "0.1"}
+
+@app.route('/db')
+def db():
+    from sqlalchemy import create_engine
+
+    engine = create_engine(config['DATABASE_URI'], echo=True)
+    rows = []
+    with engine.connect() as connection:
+        result = connection.execute("select id, name from client;")
+        rows = [dict(r.items()) for r in result]
+    return json.dumps(rows)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='80', debug=True)
